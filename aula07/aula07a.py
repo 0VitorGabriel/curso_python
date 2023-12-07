02a60434edbab36609f7d497d0b2a6ae7125eae1
0VitorGabriel <victoropo64@outlook.com>
2023-12-07 09:48:26 -0300

    aula 3 de python finalizada (operadores aritmeticos)

---
+++ aula07/aula07a.py
@@ -0,0 +1,5 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+
+print(f'A soma vale {n1 + n2}, o produto e {n1 * n2} e a divisao e {n1 / n2:.2f}', end=' >>> ')
+print(f'Divisao inteira {n1 // n2} e potencia {n1 ** n2}')
